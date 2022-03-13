e047b683329ced66426ca2c9700f10fc2b169616
Precious Chika <43546663+pre22@users.noreply.github.com>
2022-03-13 01:18:59 +0100

    updated customuser

--- accounts/managers.py
+++ accounts/managers.py
@@ -7,17 +7,15 @@ class CustomUserManager(BaseUserManager):
     Custom user model manager where email is the unique identifiers
     for authentication instead of usernames.
     """
-    def create_user(self, firstname, lastname, email, password, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         """
         Create and save a User with the given email and password.
         """
         if not email:
-            raise ValueError(_('The Email must be set'))
+            raise ValueError("User must have an email")
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
-        user.firstname = firstname
-        user.lastname = lastname
         user.save(using=self._db)
         return user
 
--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.conf import settings
 from django.db import models
 from django.utils.translation import gettext_lazy as _
@@ -24,18 +24,34 @@ STATUS = (
     ('Completed', 'Completed'),
 )
 
-class CustomUser(AbstractUser):
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    username = models.CharField(max_length=255, unique=True, null=True)
     firstname = models.CharField(max_length=200)
     lastname = models.CharField(max_length=200)
-    email = models.EmailField(_('email address'), unique=True)
     occupation = models.CharField(max_length=100, choices=OCCUPATION)
     phone = models.CharField(max_length=15)
     sex = models.CharField(max_length=20, choices=SEX)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['firstname', 'lastname', 'occupation', 'phone', 'sex']
     
     objects = CustomUserManager()
+
+    def get_full_name(self):
+        return f"{self.first_name} - {self.last_name}"
+
+    def get_short_name(self):
+        return self.username
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
 
     def __str__(self):
         return self.email
