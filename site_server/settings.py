1088c215d036fedf19dd305b7c8f99052994add4
Precious Chika <43546663+pre22@users.noreply.github.com>
2022-03-14 04:36:17 +0100

    update settings file

--- site_server/settings.py
+++ site_server/settings.py
@@ -185,7 +185,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
