001bf71a86dce8b38bfdace06452f1bcea8adb82
Precious Chika <43546663+pre22@users.noreply.github.com>
2022-03-14 04:49:35 +0100

    corrected settings

--- site_server/settings.py
+++ site_server/settings.py
@@ -194,4 +194,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-django_heroku.seetings(locals())
+django_heroku.settings(locals())
