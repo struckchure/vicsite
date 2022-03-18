ffb4cf3f6a874499642e42a4c11414b2e7c4f278
Precious Chika <43546663+pre22@users.noreply.github.com>
2022-03-18 17:53:59 +0100

    set cors to false

--- site_server/settings.py
+++ site_server/settings.py
@@ -107,9 +107,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_ALLOW_ALL = True
 
-# CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
   'http://localhost:8000',
   'https://crypto-bank-dashboard.vercel.app/',
