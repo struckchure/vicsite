929c2802275b24d504b0e47808eec8fab0c0c675
Precious Chika <43546663+pre22@users.noreply.github.com>
2022-03-18 17:55:01 +0100

    set permission to authenticated

--- site_server/settings.py
+++ site_server/settings.py
@@ -62,7 +62,7 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
 
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
