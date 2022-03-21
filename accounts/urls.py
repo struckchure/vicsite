cacb41273dfa43926e664770c3d18d733ce9b60b
Precious Chika <43546663+pre22@users.noreply.github.com>
2022-03-21 09:36:54 +0100

    imported newly created views in account app
    userasset related views

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,21 @@
 from django.urls import path, include
-from accounts.views import UserBalanceView, UserAssetView, UserAsset, CoinAddressView, CoinOptionView
+from accounts.views import (
+    UserBalanceView, 
+    UserAsset, 
+    UserAssetUpdateView, 
+    UserAssetDeleteView, 
+    CoinAddressView, 
+    CoinOptionView,
+
+)
 
 urlpatterns = [
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.authtoken")),
     path("balance/", UserBalanceView.as_view()),
     path("userassets/", UserAsset.as_view()),
-    path("userassets/edit/", UserAssetView.as_view()),
+    path("userassets/del/", UserAssetDeleteView.as_view()),
+    path("userassets/edit/", UserAssetUpdateView.as_view()),
     path("coins/", CoinOptionView.as_view()),
     path("coinaddress/<int:coin_id>/", CoinAddressView.as_view()),
 ]
