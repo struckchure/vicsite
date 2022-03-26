ae14843c6577c65242b291c2e369ce856d56d25f
Precious Chika <43546663+pre22@users.noreply.github.com>
2022-03-26 15:49:06 +0100

    Fixed Bug in Reset_Password_Confirm

--- site_server/settings.py
+++ site_server/settings.py
@@ -56,13 +56,13 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = "accounts.CustomUser"
 
 # EMAIL CONFIG
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-# EMAIL_HOST = "smtp.gmail.com"
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = env("EMAIL_HOST_USER")
-# EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
-# EMAIL_USE_TLS = env("EMAIL_USE_TLS")
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.mail.yahoo.com"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = env("EMAIL_USE_TLS")
 
 SITE_ID = 1
 
@@ -91,7 +91,7 @@ DJOSER = {
     "SET_USERNAME_RETYPE": True,
     "SET_PASSWORD_RETYPE": True,
     "USERNAME_RESET_CONFIRM_URL": "password/reset/confirm/{uid}/{token}",
-    "PASSWORD_RESET_CONFIRM_URL": "/accounts/auth/users/reset_password_confirm/{uid}2efetg34t/{token}",
+    "PASSWORD_RESET_CONFIRM_URL": "/accounts/auth/users/reset_password_confirm/{uid}/{token}",
     # "PASSWORD_RESET_CONFIRM_URL": "email/reset/confirm/{uid}2efetg34t/{token}",
     "ACTIVATION_URL": "activate/{uid}/{token}",
     "SEND_ACTIVATION_EMAIL": False,
