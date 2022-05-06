83744be3107e18c355f6a867f35e0183268b2c3e
Precious Chika <43546663+pre22@users.noreply.github.com>
2022-05-06 08:26:04 +0100

    chore: working on accounts password pages

--- site_server/urls.py
+++ site_server/urls.py
@@ -9,6 +9,13 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/signup/", SignupView, name="signup"),
     path("accounts/login/", CustomLoginView.as_view(), name="login"),
+    # path("accounts/password_change/" ),
+    # accounts/ password_change/ [name='password_change']
+    # accounts/ password_change/done/ [name='password_change_done']
+    # accounts/ password_reset/ [name='password_reset']
+    # accounts/ password_reset/done/ [name='password_reset_done']
+    # accounts/ reset/<uidb64>/<token>/ [name='password_reset_confirm']
+    # accounts/ reset/done/ [name='password_reset_complete']
     path('accounts/', include('django.contrib.auth.urls')),
     path("accounts/", include("accounts.urls")),
     path("invest/", include("investments.urls")),
