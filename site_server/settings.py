d4b6a75773274c1c2ee345b64e997fec6981f568
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2022-05-18 14:13:21 +0100

    build(docker): updates exisiting docker configs + postgres

--- site_server/settings.py
+++ site_server/settings.py
@@ -1,14 +1,9 @@
 from pathlib import Path
 import os
-from decouple import config as cfg
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
-# from site_server.ec2_check import get_linux_ec2_private_ip
 
-# private_ip = get_linux_ec2_private_ip()
-# if private_ip:
-#     ALLOWED_HOSTS.append(private_ip)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,14 +13,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = cfg("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = cfg("DEBUG", cast=bool, default=True)
+DEBUG = bool(os.environ.get("DEBUG"))
 
-ALLOWED_HOSTS = ["avaloqsassets.com"]
-# ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = ["vicsites.herokuapp.com"]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split()
 
 
 # Application definition
@@ -44,7 +37,6 @@ INSTALLED_APPS = [
     "cloudinary",
     "ckeditor",
     # "rest_framework",
-
     # Local Apps
     "accounts.apps.AccountsConfig",
     "transactions.apps.TransactionsConfig",
@@ -58,31 +50,19 @@ AUTH_USER_MODEL = "accounts.CustomUser"
 ACCOUNT_USER_MODEL_USERNAME_FIELD = None
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_USERNAME_REQUIRED = False
-ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_AUTHENTICATION_METHOD = "email"
 ACCOUNT_UNIQUE_EMAIL = True
 
 # EMAIL CONFIG
-# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp-pulse.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = cfg("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = cfg("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
 
 SITE_ID = 1
-
-# AUTHENTICATION_BACKENDS = [
-#     'django.contrib.auth.backends.ModelBackend',
-#     'allauth.account.auth_backends.AuthenticationBackend',
-# ]
-
-# Django AllAuth
-# # ACCOUNT_SIGNUP_FORM_CLASS = 'accounts.forms.CustomSignupForm'
-# ACCOUNT_FORM = {
-#     'signup': 'accounts.forms.CustomSignupForm',
-#     # 'login': 'accounts.forms.CustomLoginForm',
-# }
 
 #############
 
@@ -90,10 +70,8 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.AllowAny",
-        # 'rest_framework.permissions.IsAuthenticated',
     ],
 }
-
 
 
 MIDDLEWARE = [
@@ -115,7 +93,7 @@ ROOT_URLCONF = "site_server.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, 'templates')],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -135,18 +113,14 @@ WSGI_APPLICATION = "site_server.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": cfg("DB_NAME"),
-        "USER": cfg("DB_USERNAME"),
-        "PASSWORD": cfg("DB_PASSWORD"),
-        "HOST": cfg("DB_HOST"),
-        "PORT": cfg("DB_PORT"),
+    "default": {
+        "ENGINE": os.getenv("DB_ENGINE"),
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
+        "PORT": os.getenv("POSTGRES_PORT"),
     }
-    # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": BASE_DIR / "db.sqlite3",
-    # }
 }
 
 
@@ -198,14 +172,13 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-# django_heroku.settings(locals())
 
-cloudinary.config( 
-  cloud_name = cfg("cloud_name"), 
-  api_key = cfg("api_key"),
-  api_secret = cfg("api_secret"),
-  secure = True
+cloudinary.config(
+    cloud_name=os.environ.get("cloud_name"),
+    api_key=os.environ.get("api_key"),
+    api_secret=os.environ.get("api_secret"),
+    secure=True,
 )
 
-LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL = 'login'
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "login"
